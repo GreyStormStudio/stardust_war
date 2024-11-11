3cd78c7f23b25e8e1301aabe5198b9cba56f2335
Highmore <2300706565@qq.com>
2024-11-11 16:19:18 +0800

    写了点readme,差不多看看得了

--- src/scritps/Map-原型.ts
+++ src/scritps/Map-原型.ts
@@ -3,7 +3,7 @@ import {GenerateMap} from './快捷函数/CreateMap'
 import { defineComponent } from "vue";
 
 
-const seed = 16
+//const seed = 6
 const edge = 128
 
 const app = new Application();
@@ -15,25 +15,30 @@ await app.init({
 document.body.appendChild(app.canvas)
 let obj1 = new Graphics();
 
-function drawmap(){
+function drawmap(seed:number){
     const generater = new GenerateMap(seed)
     const map = generater.GenerateNoiseMap()
     const grayscalemap = generater.GenerateGrayscaleMap(map)
+    let tj=[0,0,0,0]
     for (let x = 0; x < edge; x++) {
         for (let y = 0; y < edge; y++) {
             let gray = grayscalemap[x+y*edge]
             let color:string
             if(gray>100){
                 color='0xFFFFFF'
+                tj[0]++
             }
             else if(gray>85){
                 color='0x00FF00'
+                tj[1]++
             }
             else if(gray>65){
                 color='0x0000FF'
+                tj[2]++
             }
             else{
                 color='0xFF0000'
+                tj[3]++
             }
             obj1.rect(x,y,1,1)
             obj1.fill({color:color})
@@ -41,9 +46,14 @@ function drawmap(){
         }
     }
     //console.log(grayscalemap)
+    console.log(`星域种子:${seed}\n星域统计:空白地块:${tj[0]}块,低产地块:${tj[1]}块,中产地块:${tj[2]}块,高产地块:${tj[3]}块\n预计产出:${tj[1]+tj[2]*2+tj[3]*4}矿石/时刻,${tj[0]*0.5}能量/时刻`)
 }
 export default defineComponent({
     setup(){
-        drawmap()
+        console.log(`单位产出: 低产地块:1矿石/时刻,中产地块:2矿石/时刻,高产地块:4矿石/时刻,空白地块:0.5能量/时刻`)
+        for (let seed = 0; seed < 8; seed++) {
+            drawmap(seed)
+        }
+        
     }
 })
---
+++ src/scritps/类/ConstellationBuildings.ts
@@ -0,0 +1,15 @@
+export class Constellationbulidings{
+    Energy_production:number
+    Mineral_production:number[]
+    Metal_protuction:number
+    constructor(
+        Energy_production:number,
+        Mineral_production:number[],
+        Metal_protuction:number
+    ){
+        this.Energy_production=Energy_production
+        this.Mineral_production=Mineral_production
+        this.Metal_protuction=Metal_protuction
+    }
+
+}
