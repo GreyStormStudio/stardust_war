9436a9267ffbde2282bc91d6f704f2872de68a21
Highmore <2300706565@qq.com>
2024-11-13 23:00:52 +0800

    增加了排名的样例

--- src/vue/游戏页/Rank.vue
+++ src/vue/游戏页/Rank.vue
@@ -1,3 +1,65 @@
 <template>
-    <p>你看你马呢</p>
-</template>
+    <div id="app">
+      <h1>排名界面</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>名字</th>
+            <th>科技</th>
+            <th>舰队</th>
+            <th>领地</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in sortedList" :key="item.id">
+            <td>{{ item.name }}</td>
+            <td>{{ item.tech }}</td>
+            <td>{{ item.fleet }}</td>
+            <td>{{ item.territory }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  
+  interface RankingItem {
+    id: number;
+    name: string;
+    tech: number;
+    fleet: number;
+    territory: number;
+  }
+  
+  export default defineComponent({
+    name: 'Ranking',
+    setup() {
+      const rankings = ref<RankingItem[]>([
+        { id: 1, name: '田所浩二', tech: 90, fleet: 85, territory: 78 },
+        { id: 2, name: '日川岗阪', tech: 85, fleet: 90, territory: 88 },
+        { id: 3, name: '梅川库刹', tech: 80, fleet: 80, territory: 90 },
+      ]);
+      return {
+        sortedList: rankings,
+      };
+    },
+  });
+  </script>
+  
+  <style scoped>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    cursor: pointer;
+  }
+  </style>
+  
