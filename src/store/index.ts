781d86350de8b6ef14b4ba1d9edd407f0db9245a
Highmore <2300706565@qq.com>
2024-11-16 11:10:31 +0800

    整了长期储存的样例

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,44 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
+
+interface PlayerState {
+  name: string;
+  score: number;
+}
+
+const state: PlayerState = {
+  name: '',
+  score: 0
+};
+
+const mutations = {
+  setPlayerName(state: PlayerState, name: string) {
+    state.name = name;
+  },
+  setPlayerScore(state: PlayerState, score: number) {
+    state.score = score;
+  }
+};
+
+const actions = {
+  updatePlayerName({ commit }: { commit: (mutation: string, payload: any) => void }, name: string) {
+    commit('setPlayerName', name);
+  },
+  updatePlayerScore({ commit }: { commit: (mutation: string, payload: any) => void }, score: number) {
+    commit('setPlayerScore', score);
+  }
+};
+
+const getters = {
+  playerName: (state: PlayerState) => state.name,
+  playerScore: (state: PlayerState) => state.score
+};
 
 export default createStore({
-  state: {
-    isgame:false
-  },
-  getters: {
-  },
-  mutations: {
-    setIsGame(state, value) {
-      state.isgame = value;
-    }
-  },
-  actions: {
-    updateIsGame({ commit }, value) {
-      commit('setIsGame', value);
-    }
-  },
-  modules: {
-  }
-})
+  state,
+  getters,
+  mutations,
+  actions,
+  modules: {},
+  plugins: [createPersistedState()] // 使用 vuex-persistedstate 插件持久化状态
+});
