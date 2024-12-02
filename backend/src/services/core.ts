ac541f7d9d9a4b80c0da90db2f1c3a3f709ef2ef
Highmore <2300706565@qq.com>
2024-12-02 16:27:50 +0800

    写了添加Ship到物理引擎的功能

--- backend/src/services/core.ts
+++ backend/src/services/core.ts
@@ -1,30 +1,37 @@
-import { SHIP } from '../../../share/CONSTANT';
+import { SHIP, SpriteEdge } from '../../../share/CONSTANT';
 import { getKey, updateData, getData } from '../db/db'
 import Matter from 'matter-js';
 
-const { Engine, Runner, Composite } = Matter
+const { Engine, Composite, Body } = Matter
 const engine = Engine.create({
     gravity: { scale: 0 },
     enableSleeping: true
 })
-export { Engine, engine }
+export { Engine, engine, Body }
 function addObject(object: Matter.Body) {//将物体添加到世界中
     Composite.add(engine.world, object)
 }
+
 export function addShip(ship: SHIP, px: number, py: number) {
-    const sinfo = { power: 0, hits: 0, mass: 0, thrust: 0, speed_hyper: 0 }
-    ship.Ship_Blocks.blocks.forEach((block) => {
+    const sinfo = { power: 0, hits: 0, mass: 0, thrust: 0, speed_hyper: 0 };
+    const ShipObject: Matter.Body[] = [Matter.Body.create(Matter.Bodies.rectangle(px, py, SpriteEdge, SpriteEdge))]//芝士核心
+    ship.Ship_Blocks.blocks.forEach(block => {//获取整船的宏观信息
         const { power, hits, mass } = block.block[block.level].baseAttribute;
-        const { x, y } = block
+        const { x, y } = block.block
+        ShipObject.push(Matter.Body.create(Matter.Bodies.rectangle(px + x * SpriteEdge, py + y * SpriteEdge, SpriteEdge, SpriteEdge)))
         sinfo.power += power;
         sinfo.hits += hits;
         sinfo.mass += mass;
         const specialAttributes = block.block[block.level].specialAttributes || {};
         sinfo.thrust += specialAttributes.thrust || 0;
         sinfo.speed_hyper += specialAttributes.speed_hyper || 0;
-    });
-
+    })
+    const Ship = Matter.Body.create({ parts: ShipObject })
+    addObject(Ship)//加到世界中
+    // 返回Ship信息
+    return sinfo;
 }
+
 //更新所有玩家的储存
 const resIncrease = { energy: 100, mineral: 50, metal: 1 }//每个资源每时刻的增长量 测试固定值
 export async function updataResource() {
--- backend/src/services/functions.ts
+++ backend/src/services/functions.ts
@@ -52,11 +52,11 @@ async function Register(email: string, username: string, password: string) {
     try {
         const seed = Math.floor(Math.random() * 128) % 128 //目前只开放128个星域
         const { x, y } = { x: Math.random() * 5000 + 60, y: Math.random() * 5000 + 60 }
+        const sinfo = addShip(c.SHIP_ZERO, x, y)
         putData(c.USER_KEY + username, {
             userinfo: { email: email, password: Md5.hashStr(password) },
-            gameinfo: { storage: { energy: 1000, mineral: 500, metal: 0 }, ship: { object: c.SHIP_ZERO, x, y, constellation: seed } }
+            gameinfo: { storage: { energy: 1000, mineral: 500, metal: 0 }, ship: { object: c.SHIP_ZERO, x, y, constellation: seed, sinfo: sinfo } }
         })//占用用户名
-        addShip(c.SHIP_ZERO, x, y)
         putData(c.EMAIL_KEY + email, { username: username, password: Md5.hashStr(password) })//占用邮箱
         return c.OK
     }
